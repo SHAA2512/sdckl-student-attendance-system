a75ace6ef9084bdc614f88ffcb8f228177427905
SHAA2512 <shaa23654@gmail.com>
2025-06-09 00:43:42 +0800

    Add run_fingerprint_service.bat 1749401022456

---
+++ run_fingerprint_service.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Batch script to start the fingerprint service server on port 8001
+
+REM Run the fingerprint service Python server with full relative path
+python sdckl-attendance-system-new\middleware\fingerprint_service.py
